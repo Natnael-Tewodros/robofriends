a0b7a54b09bcfc0cecdad485c16bd72a13f72208
Natnael-Tewodros <natnaeltewodros03@gmail.com>
2024-08-13 21:20:05 +0300

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 0,
+    name: "natnael tewodros",
+    username: "natty",
+    email: "natan@gmail.com",
+  },
+  {
+    id: 1,
+    name: "Eyael",
+    username: "eyu",
+    email: "eyu@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Eyob",
+    username: "eyoba",
+    email: "eyobajobi@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Nahom",
+    username: "james",
+    email: "james@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Eyoab",
+    username: "babi",
+    email: "eyobaase@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Adoney",
+    username: "Adoni",
+    email: "adoni@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Aleme",
+    username: "alem",
+    email: "alem@gmail.com",
+  },
+  {
+    id: 7,
+    name: "mekdes",
+    username: "mena",
+    email: "mena@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Naod",
+    username: "naye",
+    email: "naye@gmail.com",
+  },
+  {
+    id: 9,
+    name: "Betelhem",
+    username: "beti",
+    email: "beti@gmail.com",
+  },
+];
