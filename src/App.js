f325eab5b6654fb7679cef2bec8402360c4c217a
Natnael-Tewodros <natnaeltewodros03@gmail.com>
2024-08-13 23:35:21 +0300

    inital commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import SearchBox from "./SearchBox";
+import { robots } from "./robots";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "", // Corrected typo here
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value }); // Corrected typo here
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase()); // Added missing parentheses
+    });
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filteredRobots} />
+      </div>
+    );
+  }
+}
+
+export default App;
